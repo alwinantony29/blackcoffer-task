cdaadb06668903a07870e55d42c783fb76188772
Alwin Antony <110175978+alwinantony29@users.noreply.github.com>
2024-02-08 03:40:33 +0000

    organized server files

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { EnergyInsightsModule } from './energy-insights/energy-insights.module';
+import { EnergyInsightsModule } from '../modules/energy-insights/energy-insights.module';
 import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypegooseModule.forRoot(
-      'mongodb+srv://Atlas1234:Atlas123@cluster0.ysimyt6.mongodb.net/black-coffer?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@cluster0.ysimyt6.mongodb.net/black-coffer?retryWrites=true&w=majority`,
     ),
     EnergyInsightsModule,
   ],
