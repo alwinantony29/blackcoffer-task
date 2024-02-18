c8073863a7c401c25d5f12e54ed4be451fc0e13f
Alwin Antony <110175978+alwinantony29@users.noreply.github.com>
2024-02-18 13:47:40 +0000

    added navbar theme and theme toggle

---
+++ apps/client/src/components/nav-bar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='w-full h-[30px] bg-gray-800'>
+        
+    </div>
+  )
+}
+
+export default NavBar
--- apps/client/src/layouts/MainLayout.js
+++ apps/client/src/layouts/MainLayout.js
@@ -1,9 +1,18 @@
 import SideBar from '@/components/navigation/SideBar';
+import { ModeToggle } from '@/components/theme/ThemeToggle';
+import { ThemeProvider } from '@/components/theme/theme-provider';
 import React from 'react';
 
 const MainLayout = ({children}) => {
   return (
+    <ThemeProvider
+    attribute="class"
+    defaultTheme="system"
+    enableSystem
+    disableTransitionOnChange>
+
     <div className="flex">
+      <ModeToggle></ModeToggle>
       <div className="">
         <SideBar />
       </div>
@@ -11,6 +20,7 @@ const MainLayout = ({children}) => {
         {children}
       </div>
     </div>
+    </ThemeProvider>
   );
 };
 
